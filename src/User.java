6929ff5d0327cad0c69d18dc632248f62805453d
zhangruiqi2@qq.com <zhangruiqi2@qq.com>
2016-11-18 19:25:41 +0800

    user information

--- src/User.java
+++ src/User.java
@@ -1,6 +1,39 @@
 
 public class User {
-	public static void main(String[] args){
-		System.out.println("hello");
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	
+	public void setId(int id){
+		this.id=id;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setUsername(String username){
+		this.username=username;
+	}
+	
+	public String getUsername(){
+		return this.username;
+	}
+	
+	public void setPassword(String password){
+		this.password=password;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public void setEmail(String email){
+		this.email=email;
+	}
+	
+	public String getEmail(){
+		return email;
 	}
 }
